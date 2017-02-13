9092f6dade6dc032237091878a304e6a0b541cc4
MhaddenOmar <omar0954@stud.kea.dk>
2017-02-13 09:48:58 +0100

    Tsdpoijgposidkfj[

--- models/Student.java
+++ models/Student.java
@@ -13,8 +13,7 @@ package models;
 public class Student {
     private String rollNo;
     private String name;
-    private int age;
-    private int id;
+    private int age;    
    
     public String getRollNo() {
         return rollNo;
